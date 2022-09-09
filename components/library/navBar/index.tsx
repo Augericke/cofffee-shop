99d54e47da0d9440614d2f60af6207649cda7ebb
augericke <augericke@gmail.com>
2022-09-08 20:44:35 -0400

    add nav item animation

--- components/library/navBar/index.tsx
+++ components/library/navBar/index.tsx
@@ -1,13 +1,25 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Dispatch, SetStateAction } from "react";
+import { useRouter } from "next/router";
 import Link from "next/link";
-import { debounce } from "../../../utils/helpers";
 import { FaTwitter, FaGithub, FaLinkedin } from "react-icons/fa";
+import { motion, AnimateSharedLayout } from "framer-motion";
+import { debounce } from "../../../utils/helpers";
 
 const styles = require("./navBar.module.scss");
 
 type NavBarProps = {};
 
 const NavBar: React.FC<NavBarProps> = (props: NavBarProps) => {
+  // Routes
+  const { pathname } = useRouter();
+  const [selected, setSelected] = useState(pathname === "/contact" ? 3 : 0);
+  const navItems = [
+    { name: "menu", href: "/#menu" },
+    { name: "about", href: "/#about" },
+    { name: "shop", href: "/#shop" },
+    { name: "contact", href: "/contact" },
+  ];
+
   // State to hide nav on scroll down and show on scroll up
   const [prevScrollPos, setPrevScrollPos] = useState(0);
   const [scrollAtTop, setScrollAtTop] = useState(true);
@@ -75,15 +87,47 @@ const NavBar: React.FC<NavBarProps> = (props: NavBarProps) => {
         </a>
       </div>
       <div className={styles.navRoutesContainer}>
-        <span className={styles.navRoute}>menu</span>
+        <AnimateSharedLayout>
+          {navItems.map((item, key) => (
+            <NavItem
+              key={key}
+              item={item}
+              isSelected={selected === key}
+              onClick={() => setSelected(key)}
+            />
+          ))}
+        </AnimateSharedLayout>
+        {/* <span className={styles.navRoute}>menu</span>
         <span className={styles.navRoute}>about</span>
         <span className={styles.navRoute}>shop</span>
         <Link href="/contact">
           <a className={styles.navRoute}>contact</a>
-        </Link>
+        </Link> */}
       </div>
     </div>
   );
 };
 
 export default NavBar;
+
+type NavItemProps = {
+  item: { name: string; href: string };
+  isSelected: boolean;
+  onClick: any;
+};
+
+// Initial approach taken from https://framermotionplayground.com/tutorial/underlined-menu
+const NavItem: React.FC<NavItemProps> = (props: NavItemProps) => {
+  const { item, isSelected, onClick } = props;
+
+  return (
+    <div className={styles.navItem} onClick={onClick}>
+      <Link href={item.href}>
+        <a>{item.name}</a>
+      </Link>
+      {isSelected && (
+        <motion.div className={styles.navUnderline} layoutId="underline" />
+      )}
+    </div>
+  );
+};
