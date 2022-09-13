c07f14b1ce3a48b9748c270e0fbdbc866bd7a96a
augericke <augericke@gmail.com>
2022-09-13 11:54:15 -0400

    change onLoad animation of contact page

--- components/pages/contact/index.tsx
+++ components/pages/contact/index.tsx
@@ -8,41 +8,45 @@ type ContactProps = {};
 
 const Contact: React.FC<ContactProps> = (props: ContactProps) => {
   return (
-    <ShowOnViewport customClass={styles.pageContainer}>
+    <div className={styles.pageContainer}>
       <div className={styles.contactCardContainer}>
-        <div className={styles.cardTextContainer}>
-          <h1 className={styles.cardTitleText}>Trying to get in touch?</h1>
-          <p className={styles.cardBodyText}>
-            The person would like to hear suggestions on how to improve their
-            work performance
-          </p>
-        </div>
-        <div className={styles.buttonContainer}>
-          <a
-            href={"https://twitter.com/augericke"}
-            target="_blank"
-            rel="noreferrer"
-          >
-            <motion.button
-              className={styles.contactButton}
-              whileHover={{ scale: 1.05 }}
-              whileTap={{ scale: 0.95 }}
+        <ShowOnViewport customClass={styles.cardTextContainer}>
+          <>
+            <h1 className={styles.cardTitleText}>Trying to get in touch?</h1>
+            <p className={styles.cardBodyText}>
+              The person would like to hear suggestions on how to improve their
+              work performance
+            </p>
+          </>
+        </ShowOnViewport>
+        <ShowOnViewport customClass={styles.buttonContainer}>
+          <>
+            <a
+              href={"https://twitter.com/augericke"}
+              target="_blank"
+              rel="noreferrer"
             >
-              I have some suggestions
-            </motion.button>
-          </a>
-          <a href="mailto:austingericke.inquiries@gmail.com">
-            <motion.button
-              className={styles.contactButton}
-              whileHover={{ scale: 1.05 }}
-              whileTap={{ scale: 1 }}
-            >
-              I require a direct line to your creator
-            </motion.button>
-          </a>
-        </div>
+              <motion.button
+                className={styles.contactButton}
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 0.95 }}
+              >
+                I have some suggestions
+              </motion.button>
+            </a>
+            <a href="mailto:austingericke.inquiries@gmail.com">
+              <motion.button
+                className={styles.contactButton}
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 1 }}
+              >
+                I require a direct line to your creator
+              </motion.button>
+            </a>
+          </>
+        </ShowOnViewport>
       </div>
-    </ShowOnViewport>
+    </div>
   );
 };
 
